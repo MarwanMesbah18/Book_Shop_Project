a3f0cb1d640e16507247de5fcec1557ff6233702
20102 <20102@LAPTOP-4MB5EUFM>
2025-05-09 12:36:13 +0300

    add class shopping cart

---
+++ src/main/java/Clasess/ShoppingCart.java
@@ -0,0 +1,14 @@
+package Clasess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+	
+		List<CartItem> item= new ArrayList<>();
+
+		public List<CartItem> getItem() {
+			return item;
+
+	}
+}
