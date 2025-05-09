a1b959f936e1fe096c5f17e9ca34cdc82f630d4b
Marwan Mesbah <marwan.2005.omar@gmail.com>
2025-05-09 12:17:58 +0300

    cartitem class

---
+++ src/main/java/Clasess/CartItem.java
@@ -0,0 +1,31 @@
+package Clasess;
+
+public class CartItem {
+	private Books book;
+	private int quantity;
+
+	public CartItem(Books book, int quantity) {
+		this.book = book;
+		this.quantity = quantity;
+	}
+
+	public Books getBook() {
+		return book;
+	}
+
+	public void setBook(Books book) {
+		this.book = book;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getTotalPrice() {
+		return book.getPrice() * quantity;
+	}
+}
